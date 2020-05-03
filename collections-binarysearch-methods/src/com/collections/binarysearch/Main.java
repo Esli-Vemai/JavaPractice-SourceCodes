4fb1e417316a513cb88ddb387d3372e5173f261a
Esli-Vemai <eslivemai@gmail.com>
2020-05-03 21:46:55 +0530

    first commit

---
+++ collections-binarysearch-methods/src/com/collections/binarysearch/Main.java
@@ -0,0 +1,19 @@
+package com.collections.binarysearch;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Theatre theatre = new Theatre("Olympian", 8, 12);
+//        theatre.getSeats();
+		if (theatre.reserveSeat("B111")) {
+			System.out.println("Please pay");
+		} else {
+			System.out.println("Sorry, seat is taken");
+		}
+		if (theatre.reserveSeat("B11")) {
+			System.out.println("Please pay");
+		} else {
+			System.out.println("Sorry, seat is taken");
+		}
+	}
+}
